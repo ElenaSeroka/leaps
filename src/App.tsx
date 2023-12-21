bbc9ad5e2c99b8d5b4fb0f46dfcf3c692669eb75
elena_seroka <70527748+ElenaSeroka@users.noreply.github.com>
2023-12-21 15:50:30 +0100

    Folder for homepage added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './home-page';
+import HomePage from './HomePage/home-page';
 import DashBoard from './Dashboard/dashboard';
 import TopNavBar from './TopNavBar/topnav-bar';
 
