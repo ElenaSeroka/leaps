122a28766f4c7b0a0927fd12b4978e0919656db1
SeanLangre <conk3r_xd@hotmail.com>
2023-12-21 17:33:58 +0100

    created tailwind buttons for dashboard

--- src/Layouts/Dashboard/dashboard.tsx
+++ src/Layouts/Dashboard/dashboard.tsx
@@ -7,16 +7,39 @@ const DashBoard: React.FC = () => {
     return (
         <nav className="Dashboard">
             <ul>
-                <li>Leaps</li>
-                <li>Daily Goals</li>
-                <li>Reports</li>
-                <li>Goals setting</li>
-                <li>Add tracker</li>
-                <li>Settings</li>
-                <li>Log out</li>
                 <li>
-                    <button onClick={() => { /* Handle button click here */ }}>
-                        Your Button Text
+                    <h1 className="text-4xl font-bold text-center text-indigo-800">
+                        Leaps
+                    </h1>
+                </li>
+                <li>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded opacity-50">
+                        Daily Goals
+                    </button>
+                </li>
+                <li>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Reports
+                    </button>
+                </li>
+                <li>
+                    <h2 className="text-4xl font-bold text-center text-indigo-800">
+                        Goals setting
+                    </h2>
+                </li>
+                <li>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded opacity-50">
+                        Add tracker
+                    </button>
+                </li>
+                <li>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded opacity-50">
+                        Settings
+                    </button>
+                </li>
+                <li>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded opacity-50">
+                        Log out
                     </button>
                 </li>
             </ul>
